8e67905e4c9ee3d4e2a062c71fa89ba1b0d77a7c
TylerMcKenna <101288310+TylerMcKenna@users.noreply.github.com>
2025-02-11 19:36:49 -0600

    Complete excersizes 01-06

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(intArray, ...searchValues) {
+    return intArray.filter(num => !searchValues.includes(num))
 };
 
 // Do not edit below this line
