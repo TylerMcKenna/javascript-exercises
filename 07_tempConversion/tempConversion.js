485e32599fda3e4efa97c72b7169ecc21a2e3558
TylerMcKenna <101288310+TylerMcKenna@users.noreply.github.com>
2025-02-11 20:01:06 -0600

    Complete excersize 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheitTemp) {
+  const convertedFarenheitValue = (farenheitTemp - 32) * (5/9);
+  return Math.round(convertedFarenheitValue * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celciusTemp) {
+  const convertedCelciusValue = (celciusTemp * (9/5) + 32);
+  return Math.round(convertedCelciusValue * 10) / 10;
 };
 
 // Do not edit below this line
