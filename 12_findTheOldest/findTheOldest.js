a04ac035fca906ff09290579a2a1d44eabc7d223
TylerMcKenna <101288310+TylerMcKenna@users.noreply.github.com>
2025-02-21 22:52:19 -0600

    Complete through 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let indexOfOldest = 0;
+    let ageOfOldest = -1;
+    for (let i = 0; i < people.length; i++) {
+        let currentAge;
+        let person = people[i];
+        if (!("yearOfDeath" in person)) {
+            currentAge = 2025 - person.yearOfBirth
+        } else {
+            currentAge = person.yearOfDeath - person.yearOfBirth;
+        }
+        
+        if (ageOfOldest < currentAge) {
+            ageOfOldest = currentAge;
+            indexOfOldest = i;
+        }
+    }
+    return people[indexOfOldest];
 };
 
 // Do not edit below this line
